414d62326d022bcb5e7de5f69c2405f14e1c3e19
Sasha Turchanskiy <turchanskiy777@gmail.com>
2025-06-17 12:48:32 +0300

    finished implementing task service

--- src/main/java/com/alekdev/TaskApp/service/impl/TaskServiceImpl.java
+++ src/main/java/com/alekdev/TaskApp/service/impl/TaskServiceImpl.java
@@ -4,6 +4,7 @@ import com.alekdev.TaskApp.dto.Response;
 import com.alekdev.TaskApp.dto.TaskRequest;
 import com.alekdev.TaskApp.entity.Task;
 import com.alekdev.TaskApp.entity.User;
+import com.alekdev.TaskApp.enums.Priority;
 import com.alekdev.TaskApp.exception.NotFoundException;
 import com.alekdev.TaskApp.repo.TaskRepo;
 import com.alekdev.TaskApp.service.TaskService;
@@ -12,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -51,6 +53,7 @@ public class TaskServiceImpl implements TaskService {
     }
 
     @Override
+    @Transactional
     public Response<List<Task>> getAllMyTasks() {
         log.info("Fetching all tasks for the current user");
 
@@ -77,21 +80,69 @@ public class TaskServiceImpl implements TaskService {
 
     @Override
     public Response<Task> updateTask(TaskRequest taskRequest) {
-        return null;
+        log.info("Updating task with id: {}", taskRequest.getId());
+
+        Task task = taskRepo.findById(taskRequest.getId()).orElseThrow(()-> new NotFoundException("Task not found with id: " + taskRequest.getId()));
+
+        if (taskRequest.getTitle() != null) task.setTitle(taskRequest.getTitle());
+        if (taskRequest.getDescription() != null) task.setDescription(taskRequest.getDescription());
+        if (taskRequest.getCompleted() != null) task.setCompleted(taskRequest.getCompleted());
+        if (taskRequest.getPriority() != null) task.setPriority(taskRequest.getPriority());
+        if (taskRequest.getDueDate() != null) task.setDoeDate(taskRequest.getDueDate());
+        task.setUpdatedAt(LocalDateTime.now());
+
+        // Save the updated task
+        Task updatedTask = taskRepo.save(task);
+
+        return Response.<Task>builder()
+                .statusCode(200)
+                .message("Task updated successfully")
+                .data(updatedTask)
+                .build();
     }
 
     @Override
     public Response<Void> deleteTask(Long taskId) {
-        return null;
+        log.info("Deleting task with id: {}", taskId);
+
+        if (!taskRepo.existsById(taskId)) {
+            throw new NotFoundException("Task not found with id: " + taskId);
+        }
+        taskRepo.deleteById(taskId);
+        return Response.<Void>builder()
+                .statusCode(200)
+                .message("Task deleted successfully")
+                .build();
     }
 
     @Override
+    @Transactional
     public Response<List<Task>> getMyTasksByCompletionStatus(boolean completed) {
-        return null;
+        log.info("inside getMyTasksByCompletionStatus()");
+
+        User currentUser = userService.getCurrentLoggedInUser();
+        List<Task> tasks = taskRepo.findByCompletedAndUser(completed, currentUser);
+
+        return Response.<List<Task>>builder()
+                .statusCode(200)
+                .message("Tasks filtered by completion status retrieved successfully")
+                .data(tasks)
+                .build();
     }
 
     @Override
     public Response<List<Task>> getMyTasksByPriority(String priority) {
-        return null;
+        log.info("inside getMyTasksByPriority()");
+
+        User currentUser = userService.getCurrentLoggedInUser();
+
+        Priority priorityEnum = Priority.valueOf(priority.toUpperCase());
+        List<Task> tasks = taskRepo.findByPriorityAndUser(priorityEnum, currentUser, Sort.by(Sort.Direction.DESC, "id"));
+
+        return Response.<List<Task>>builder()
+                .statusCode(200)
+                .message("Tasks filtered by priority retrieved successfully")
+                .data(tasks)
+                .build();
     }
 }
